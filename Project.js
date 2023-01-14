d22c21760c7842feb6c6cbee66e0e76c1252d96d
uttam kumar patra <dev.uttampatra@gmail.com>
2023-01-14 12:02:50 +0530

    links done

--- Project.js
+++ Project.js
@@ -3,6 +3,26 @@ project.innerHTML =`
 <h1 class="title" id="">Projects</h1>
     <div class="projects">
         <div class="project_image">
+            <img src="https://media.licdn.com/dms/image/C5622AQEjC5MsGznoGA/feedshare-shrink_800/0/1672754487832?e=1676505600&v=beta&t=TO0VFTbUK8wjOPH5GgWNTqKlrwZyMra8B10hOzk8uPQ">
+            <div class="tech_stacks">
+                <span>Tech-stacks : HTML, CSS, Javascript</span>
+            </div>
+        </div>
+        <div class="project_details">
+            <h2>Netflix</h2>
+            <p>ectetur adipisicing elit. Molestias quaerat sed vero suscipit.</p>
+            <p>e. Nulla nobis laborum eveniet harum similique temporibus, sequi dolorum hic error corrupti.</p>
+            <h4>Features</h4>
+            <p>Responsive web design, Letest movies</p>
+            <div class="action">
+                <a href="https://uttampatra21.github.io/Netflix/">Live</a>
+                <a href="https://github.com/uttampatra21/Netflix">Github</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="projects">
+        <div class="project_image">
             <img src="https://media.licdn.com/dms/image/C4D22AQGGhaNpGY7ZpQ/feedshare-shrink_800/0/1670395934033?e=1676505600&v=beta&t=snklBVKfTFx_mGIu2y0ochH9mXuBBztpBbh2fNUzPC4">
             <div class="tech_stacks">
                 <span>Tech-stacks : HTML, CSS, Javascript</span>
@@ -17,26 +37,6 @@ project.innerHTML =`
             <div class="action">
                 <a href="#">Live</a>
                 <a href="https://github.com/uttampatra21/cera-ecommerce">Github</a>
-            </div>
-        </div>
-    </div>
-
-    <div class="projects">
-        <div class="project_image">
-            <img src="https://media.licdn.com/dms/image/C5622AQEjC5MsGznoGA/feedshare-shrink_800/0/1672754487832?e=1676505600&v=beta&t=TO0VFTbUK8wjOPH5GgWNTqKlrwZyMra8B10hOzk8uPQ">
-            <div class="tech_stacks">
-                <span>Tech-stacks : HTML, CSS, Javascript</span>
-            </div>
-        </div>
-        <div class="project_details">
-            <h2>Netflix</h2>
-            <p>ectetur adipisicing elit. Molestias quaerat sed vero suscipit.</p>
-            <p>e. Nulla nobis laborum eveniet harum similique temporibus, sequi dolorum hic error corrupti.</p>
-            <h4>Features</h4>
-            <p>Responsive web design, Letest movies</p>
-            <div class="action">
-                <a href="#">Live</a>
-                <a href="#">Github</a>
             </div>
         </div>
     </div>
